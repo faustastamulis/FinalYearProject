9fb12115306260c5425b4824698da2bb9bba8a14
faustastamulis <g00373028@gmit.ie>
2023-04-20 02:22:32 +0100

    Added content to the about page

--- medical-app/src/components/pages/About.js
+++ medical-app/src/components/pages/About.js
@@ -2,27 +2,58 @@ import React from 'react';
 
 function Aboutus() {
     return (
-        <div style={{
-            textAlign: "center",
-            margin: "0 auto",
-            border: "1px solid #e6e6e6",
-            padding: "40px 25px",
-            marginTop: "50px",
-            background: "#646464"
-        }}>
+        <body>
+            <h2 className="h2__main">Welcome to ATU Medical</h2>
+            <div className="about__container">
+                <div className="about__text">
+                    <h4>Here we make booking appointments easy for everyone,
+                        Here at ATU Medical we strive for innovation by enabling our patients to log on and create their appointments all at their convenience.
+                        <br />
+                        <br />
+                        We do this so it makes patients feel comfortable when booking appointment. Our patients will no longer have to ring up to get an appointment,
+                        with a touch of a button you can register and make an account which lets you access the booking appointment section.</h4>
+                </div>
+                <div className="atu__image">
+                    <img src={require('../images/atuimage.jpg')} />
+                </div>
+            </div>
+            <br />
+            <div className="about__container2">
 
-            <h1 style={{ color: "#FFF" }}>ATU Medical</h1>
-            <p style={{
-                lineHeight: 1.5,
-                fontWeight: 300,
-                marginBottom: "25px",
-                fontSize: "1.375rem",
-                color: "#FFF"
-            }}>This website is purpose built for making booking appointments for medical institutes easier,
-                you simply register make an account and can create appointments.</p>
-
-
-        </div>
+                <div className="atu__image2">
+                    <img src={require('../images/atuimage2.jpg')} />
+                </div>
+                <div className="about__text2">
+                    <h4>ATU Medical is an exceptional medical facility that provides top-quality healthcare services to patients in a safe, comfortable, and welcoming environment.
+                        <br />
+                        <br />
+                        The facility is staffed by highly skilled and compassionate healthcare professionals who are dedicated to delivering exceptional medical care and support to patients.
+                        The facility boasts state-of-the-art medical equipment and technology, which allows for accurate and efficient diagnosis and treatment.
+                        <br />
+                        <br />
+                        The facility is also designed with the patient in mind, featuring comfortable waiting areas, private treatment rooms, and amenities such as Wi-Fi, television, and refreshments.
+                    </h4>
+                    <br />
+                </div>
+            </div>
+            <div className="about__container3">
+                <div className="about__text3">
+                    <h4>ATU Medical offers a wide range of medical services, including preventative care, general medical services, and complex procedures.
+                        The facility has a team of specialists in various fields who work collaboratively to meet the diverse needs of patients.
+                        <br />
+                        <br />
+                        ATU Medical prioritizes patient education and provides resources to help patients better understand their conditions and treatment options.
+                        ATU Medical is committed to patient safety and infection control, with strict protocols in place to prevent the spread of illness or disease.
+                        <br />
+                        <br />
+                        The facility adheres to the highest standards of hygiene, including frequent hand washing, sterilization of equipment, and proper disposal of medical waste.
+                    </h4>
+                </div>
+                <div className="atu__image3">
+                    <img src={require('../images/atuimage3.jpg')} />
+                </div>
+            </div>
+        </body>
     );
 
 }
--- medical-app/src/components/pages/Home.js
+++ medical-app/src/components/pages/Home.js
@@ -6,19 +6,23 @@ import { Container } from 'react-bootstrap';
 function Home() {
     return (
         <body className='homebody'>
-            <Container className="homecontainer">
-            <h1 className='homeh1'>Welcome to ATU Medical</h1>
-            <h3 className='homeh3'>Here we make booking appointments easy</h3>
-            <br />
-            <Link to="/register">
-                <button className='homebutton'>Lets get started</button>
-            </Link>
-            <br />
-            <Link to="/about">
-                <button className='homebutton'>More About Us</button>
-            </Link>
-            <br />
-            </Container>
+            <div className="homecontainer">
+                <h1 className='homeh1'>Welcome to ATU Medical</h1>
+                <h3 className='homeh3'>Here we make booking appointments easy</h3>
+                <br />
+                <div className='started__btn'>
+                <Link to="/register">
+                    <button className='homebutton'>Lets get started</button>
+                </Link>
+                </div>
+                <br />
+                <div className='about__btn'>
+                <Link to="/about">
+                    <button className='homebutton'>More About Us</button>
+                </Link>
+                </div>
+                <br />
+            </div>
         </body>
     );
 }
